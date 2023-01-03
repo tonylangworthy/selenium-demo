53a09245ab2aff33759028df78c16484658bd328
Tony Langworthy <tony.langworthy@employbridge.com>
2023-01-03 15:43:40 -0600

    TWL - Test that verifies that content is added to the WYSIWYG editor.

--- src/test/java/org/example/WysiwygEditorFT.java
+++ src/test/java/org/example/WysiwygEditorFT.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.time.Duration;
 import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,26 +16,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WysiwygEditorFT {
 
-    @Test
-    public void verifyFreeTrialIsDisplayed(){
+    private static WebDriver driver;
 
-        WebDriver driver = new ChromeDriver();
+    @BeforeAll
+    static void setup() {
+
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("http://the-internet.herokuapp.com/tinymce");
+    }
+
+    @Test
+    public void testingWysiwygEditorVerifiesThatContentIsAdded(){
 
         String expectedContent = "Here is some content!";
 
-        WebElement fullNameElement = driver.findElement(By.id("tinymce"));
-        fullNameElement.sendKeys(expectedContent);
+        driver.switchTo().frame(0);
 
-        System.out.println("Text: " + fullNameElement.getText());
+        WebElement wysiwygElement = driver.findElement(By.id("tinymce"));
+        wysiwygElement.clear();
+        wysiwygElement.sendKeys(expectedContent);
 
-//        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(10))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.id("tinymce")));
+        String actualContent = driver.findElement(By.id("tinymce")).getText();
 
-//        String actualFullName = firstResult.getText();
-
-//        assertEquals(expectedContent, actualFullName);
+        assertEquals(expectedContent, actualContent);
     }
 }
