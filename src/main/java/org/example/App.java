d35c968dc82c07b7a9af89885862cf5905c13f61
Tony Langworthy <tony.langworthy@employbridge.com>
2023-01-03 15:59:26 -0600

    TWL - Removed test code from the main class.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,9 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 
 /**
  * Hello world!
@@ -19,39 +16,5 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-
     }
-
-    @Test
-    public void verifyFreeTrialIsDisplayed(){
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://portal.mms.mckesson.com/contact-us");
-
-        String expectedFullName = "Tony L";
-
-        WebElement fullNameElement = driver.findElement(By.name("contactUsFullName"));
-        fullNameElement.sendKeys(expectedFullName);
-
-        System.out.println("Text: " + fullNameElement.getText());
-
-        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("contactUsFullName")));
-
-        String actualFullName = firstResult.getText();
-
-        Assert.assertEquals(expectedFullName, actualFullName);
-    }
-
-    @Test
-    public void verifyCoursePreviewIsDisplayed(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
-        Assert.assertTrue(driver.findElement(By.xpath("//div[@id='course-page-hero']//div[@class='ps-button section'][2]"))
-                .isDisplayed());
-    }
-
 }
